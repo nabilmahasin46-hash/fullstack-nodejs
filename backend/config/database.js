b7a77ea53ab82a64b9f9ba7ea98fb3f907110512
NabilMahasin <nabilmahasin46@gmail.com>
2026-05-12 11:55:38 +0700

    dsdd

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,10 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 const db = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD, 
+    process.env.MYSQLDATABASE || process.env.DB_NAME, 
+    process.env.MYSQLUSER || process.env.DB_USER, 
+    process.env.MYSQLPASSWORD || process.env.DB_PASSWORD, 
     {
-        host: process.env.MYSQLHOST,
+        host: process.env.MYSQLHOST || process.env.DB_HOST,
         port: process.env.MYSQLPORT || 3306,
         dialect: "mysql"
-        
     }
 );
-
-export default db;
