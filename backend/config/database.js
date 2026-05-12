de41fda4e3bb507d28720ee45e026a4ac9b34344
NabilMahasin <nabilmahasin46@gmail.com>
2026-05-12 11:45:53 +0700

    deploy

--- backend/config/database.js
+++ backend/config/database.js
@@ -8,8 +8,8 @@ const db = new Sequelize(
     process.env.DB_USER, 
     process.env.DB_PASSWORD, 
     {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT || 3306,
+        host: process.env.MYSQLHOST,
+        port: process.env.MYSQLPORT || 3306,
         dialect: "mysql"
         
     }
