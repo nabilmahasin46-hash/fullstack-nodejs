adb8df2f63e0893e1fc7afeb328b679ee466d43a
Developer <dev@example.com>
2026-06-15 14:08:52 +0700

    reuplop

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,6 +9,7 @@ const db = new Sequelize(
     process.env.DB_PASSWORD || '',
     {
         host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 3306,
         dialect: 'mysql'
     }
 );
